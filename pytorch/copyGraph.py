baf804b07b78a6ab6c41a5a4fc6c1cf806b5014a
Johnson Zhong <zhsh@umich.edu>
2019-04-01 21:07:14 -0400

    Clean up code

--- pytorch/copyGraph.py
+++ pytorch/copyGraph.py
@@ -4,7 +4,7 @@ import h5py
 import numpy as np
 import argparse
 import scipy.io as sio
-from config import get_data_dir, get_output_dir
+from config import get_data_dir
 
 # python 3 compatibility
 try:
@@ -15,7 +15,8 @@ except ImportError:
 # Note that just like in RCC & RCC-DR, the graph is built on original data.
 # Once the features are extracted from the pretrained SDAE,
 # they are merged along with the mkNN graph data into a single file using this module.
-parser = argparse.ArgumentParser(description='This module is used to merge graph and extracted features into single file')
+parser = argparse.ArgumentParser(
+    description='This module is used to merge graph and extracted features into single file')
 parser.add_argument('--data', dest='db', type=str, default='mnist', help='name of the dataset')
 parser.add_argument('--graph', dest='g', help='path to the graph file', default=None, type=str)
 parser.add_argument('--features', dest='feat', help='path to the feature file', default=None, type=str)
@@ -25,9 +26,9 @@ parser.add_argument('--h5', dest='h5', action='store_true', help='to store as h5
 if __name__ == '__main__':
     args = parser.parse_args()
 
+
 def main(args):
     datadir = get_data_dir(args.db)
-    outputdir = get_output_dir(args.db)
 
     featurefile = os.path.join(datadir, args.feat)
     graphfile = os.path.join(datadir, args.g)
@@ -37,22 +38,22 @@ def main(args):
         if args.h5:
             data0 = h5py.File(featurefile, 'r')
             data1 = h5py.File(graphfile, 'r')
-            data2 = h5py.File(outputfile+'.h5','w')
+            data2 = h5py.File(outputfile + '.h5', 'w')
         else:
-            fo = open(featurefile,'rb')
+            fo = open(featurefile, 'rb')
             data0 = pickle.load(fo)
             data1 = sio.loadmat(graphfile)
             fo.close()
 
-        x0 = data0['data'][:].astype(np.float32).reshape((len(data0['labels'][:]),-1))
-        x1 = data1['X'][:].astype(np.float32).reshape((len(data1['gtlabels'].T),-1))
+        x0 = data0['data'][:].astype(np.float32).reshape((len(data0['labels'][:]), -1))
+        x1 = data1['X'][:].astype(np.float32).reshape((len(data1['gtlabels'].T), -1))
 
-        a,b = np.where(x0 - x1)
+        a, b = np.where(x0 - x1)
         assert not a.size
 
-        joined_data = {'gtlabels':data0['labels'][:], 'X':data0['data'][:].astype(np.float32),
-                                            'Z':data0['Z'][:].astype(np.float32),
-                                            'w':data1['w'][:].astype(np.float32)}
+        joined_data = {'gtlabels': data0['labels'][:], 'X': data0['data'][:].astype(np.float32),
+                       'Z': data0['Z'][:].astype(np.float32),
+                       'w': data1['w'][:].astype(np.float32)}
 
         if args.h5:
             data2.create_dataset('gtlabels', data=data0['labels'][:])
@@ -63,8 +64,8 @@ def main(args):
             data1.close()
             data2.close()
         else:
-            sio.savemat(outputfile+'.mat', joined_data)
+            sio.savemat(outputfile + '.mat', joined_data)
         return joined_data
     else:
         print('one or both the files not found')
-        raise
+        raise FileNotFoundError
